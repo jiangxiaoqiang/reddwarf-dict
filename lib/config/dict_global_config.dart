9371e661d28ac72b4d412af4ba82ba9a3c3269cc
jiang qiang <jiangtingqiang@gmail.com>
2021-11-04 12:06:14 +0800

    [feature] tweak

--- lib/config/dict_global_config.dart
+++ lib/config/dict_global_config.dart
@@ -5,17 +5,18 @@ import 'package:biyi_app/networking/local_db/local_db.dart';
 import 'package:biyi_app/utilities/config.dart';
 import 'package:biyi_app/utilities/env.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:wheel/wheel.dart' show AppLogHandler, GlobalConfig,ConfigType;
+import 'package:wheel/wheel.dart' show AppLogHandler, GlobalConfig, ConfigType;
 
 import '../includes.dart';
 
 final pageStorageBucket = PageStorageBucket();
 
 class DictGlobalConfig {
-  static void loadApp(ConfigType configType) async{
+  static void loadApp(ConfigType configType) async {
     void _handleError(Object obj, StackTrace stack) {
-      AppLogHandler.restLogger("global error" + stack.toString());
+      AppLogHandler.restLoggerException("global error", stack, obj);
     }
+
     runZonedGuarded(() async {
       GlobalConfig.init(configType);
       await ProAccount.instance.ensureInitialized();
@@ -25,15 +26,9 @@ class DictGlobalConfig {
       FlutterError.onError = (FlutterErrorDetails errorDetails) {
         AppLogHandler.logFlutterErrorDetails(errorDetails);
       };
-      runApp(
-          AppNavigator()
-      );
-    },(Object error,StackTrace stackTrace){
-      _handleError(error,stackTrace);
+      runApp(AppNavigator());
+    }, (Object error, StackTrace stackTrace) {
+      _handleError(error, stackTrace);
     });
   }
 }
-
-
-
-
